2a87328d81c7bbd7b8e46998465fe9efae84ff8e
Bingobalazs <baluvilaga@gmail.com>
2025-01-31 17:58:26 +0100

    Revert "frontend init"
    This reverts commit c23e8312041d6efc6d366026c66a94b0554298b4.

--- blabber/android/app/src/main/kotlin/moriczcloud/hu/blabber/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package moriczcloud.hu.blabber
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- blabber/android/app/src/main/kotlin/moriczloud/hu/blabber/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package moriczloud.hu.blabber
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
