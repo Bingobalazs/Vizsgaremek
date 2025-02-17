284d19e1c4d5386b7f5942600f8d9594417b5c55
Balázs Gál <103860334+Bingobalazs@users.noreply.github.com>
2025-02-17 10:06:39 +0100

    uj ip heee

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -2,7 +2,7 @@
 return [
     'paths' => ['api/*', '/PixelArtSpotlight/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:58053/', 'https://balgalazs.moriczcloud.hu'],
+    'allowed_origins' => ['http://localhost:58053/', 'https://balgalazs.moriczcloud.hu', 'http://localhost:59964/'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
