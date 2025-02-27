9326c2be9e0e6705f353a64b558c7533e6449b4a
Balázs Gál <103860334+Bingobalazs@users.noreply.github.com>
2025-02-27 12:39:49 +0100

    das

--- blabber/lib/main.dart
+++ blabber/lib/main.dart
@@ -2,13 +2,45 @@ import 'package:blabber/screens/friend_suggestions.dart';
 import 'package:blabber/screens/user_profile_screen.dart';
 import 'package:blabber/screens/auth_screen.dart';
 import 'package:blabber/screens/identicard_form.dart';
+import 'package:blabber/screens/add_post_screen.dart';
+import 'package:blabber/screens/loading_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    title: 'Navigation Basics',
-    home: FirstRoute(),
-  ));
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Color.fromRGBO(255, 32, 78, 1), // Background color
+            foregroundColor: Color.fromRGBO(255, 255, 255, 1), // Text color
+            padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(0), // Rounded corners
+            ),
+            elevation: 3, // Shadow effect
+          ),
+        ),
+
+        primaryColor:  Color.fromRGBO(0, 34, 77, 1),
+        scaffoldBackgroundColor: Color.fromRGBO(0, 34, 77, 1), // Change this for app background
+        appBarTheme: AppBarTheme(
+          backgroundColor:  Color.fromRGBO(255, 32, 78, 1),
+          foregroundColor: Colors.white
+        )
+
+
+        
+      ),
+      home: const LoadingScreen(),
+    );
+  }
 }
 
 class FirstRoute extends StatelessWidget {
@@ -85,4 +117,102 @@ class FirstRoute extends StatelessWidget {
   }
 }
 
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  // List of pages to navigate to
+  final List<Widget> _pages = [
+     LoginPage(),
+    const IdCardForm(),
+    const AddPostScreen(),
+    const FriendSuggestionsScreen(),
+    const ProfilePage(),
+  ];
+
+  void _onItemTapped(int index) {
+    if (index != 2) {
+      // If not the add post button
+      setState(() {
+        _selectedIndex = index;
+      });
+    } else {
+      // Navigate to add post screen with a different animation
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => const AddPostScreen(),
+        ),
+      );
+    }
+  }
+  final selectedColor = Color.fromRGBO(255, 32, 78, 1);
+  final baseColor =  Color.fromRGBO(0, 34, 77, 1);
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomAppBar(
+        color: baseColor,
+        
+        child: Container(
+          height: 60.0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(
+                  Icons.home,
+                  color: _selectedIndex == 0 ? selectedColor : Colors.white,
+                ),
+                onPressed: () => _onItemTapped(0),
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.search,
+                  color: _selectedIndex == 1 ? selectedColor : Colors.white,
+                ),
+                onPressed: () => _onItemTapped(1),
+              ),
+              const SizedBox(width: 40.0), // Space for the FAB
+              IconButton(
+                icon: Icon(
+                  Icons.message,
+                  color: _selectedIndex == 3 ? selectedColor : Colors.white,
+                ),
+                onPressed: () => _onItemTapped(3),
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.person,
+                  color: _selectedIndex == 4 ? selectedColor : Colors.white,
+                ),
+                onPressed: () => _onItemTapped(4),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _onItemTapped(2),
+        backgroundColor: selectedColor,
+        elevation: 8.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(0),
+        ),
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  
+    
+  }
+
+}
---
+++ blabber/lib/screens/add_post_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+  @override
+  void initState() {
+    
+  }
+class AddPostScreen extends StatelessWidget {
+  const AddPostScreen({super.key});
+
+   @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Center(child: Text('Hello World')),
+    );
+
+  }
+}
