4d946a61949a3762e2089b24b7d883479b558b12
KCsaba1 <cskovacs4@gmail.com>
2025-03-17 20:33:19 +0100

    2.1.6

--- backend/app/Http/Controllers/AutoController.php
+++ backend/app/Http/Controllers/AutoController.php
@@ -10,8 +10,8 @@ use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 
-use App\Models\Chat;
 use App\Models\Auto;
+use App\Models\Chat;
 
 class AutoController extends Controller
 {
