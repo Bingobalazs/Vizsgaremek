bfea065a880553eccaac037b0b744faa5430a559
Bingobalazs <baluvilaga@gmail.com>
2025-03-18 17:03:14 +0100

    post api docs + dependency fix

--- backend/app/Models/chat.php
+++ backend/app/Models/chat.php
@@ -1,21 +1,15 @@
 <?php
+
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Chat extends Model
 {
-    use HasFactory;
-    protected $table = "chat";
-    protected $primaryKey = 'id';
+    protected $table = 'chat';
+    protected $fillable = [
+        'from_id', 'to_id', 'chat'
+    ];
     public $timestamps = true;
 
-    protected $fillable = [
-        'from_id',
-        'to_id',
-        'chat',
-    ];
-
-    protected $dates = ['created_at', 'updated_at'];
 }
