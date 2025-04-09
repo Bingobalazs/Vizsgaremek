7c88c36c7862c5580d01fd94bd375b7d6791078c
Balázs Gál <103860334+Bingobalazs@users.noreply.github.com>
2025-04-09 11:44:01 +0200

    anyad

--- backend/app/Http/Controllers/PostController.php
+++ backend/app/Http/Controllers/PostController.php
@@ -80,7 +80,7 @@ class PostController extends Controller
     public function store(Request $request)
     {
 
-        dd($request->all());
+
         $validated = $request->validate([
             'content' => 'required|string',
             'media_url' => 'image|mimes:jpeg,png,jpg,gif|max:5048', // max 5MB
@@ -103,7 +103,7 @@ class PostController extends Controller
             $filename = time().'_'.$file->getClientOriginalName();
             $imagePath = $file->storeAs('uploads', $filename);
 
-
+            dd($imagePath);
         } else $imagePath=null;
 
 
