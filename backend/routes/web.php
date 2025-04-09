e669118693f63290d0286c5b8754276b45784842
Balázs Gál <103860334+Bingobalazs@users.noreply.github.com>
2025-04-09 11:53:12 +0200

    nemleszjo

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -4,8 +4,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\AutoController;
+
 use App\Http\Controllers\FriendsController;
 use App\Http\Controllers\SiteController;
 use App\Http\Controllers\NewPasswordController;
