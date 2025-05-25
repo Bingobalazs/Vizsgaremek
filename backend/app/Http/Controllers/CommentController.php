e818e3a8eba2d4895728632a3f6c50a07c3c2361
KCsaba1 <cskovacs4@gmail.com>
2025-05-25 20:39:02 +0200

    Update CommentController.php

--- backend/app/Http/Controllers/CommentController.php
+++ backend/app/Http/Controllers/CommentController.php
@@ -41,16 +41,17 @@ class CommentController extends Controller
     {
         $user = Auth::user();
 
-        /*$validated = $request->validate([
+        $validated = $request->validate([
             'post_id' => 'required|integer',
             'comment' => 'required|string',
-        ]);*/
-        //$validated['user_id'] = $user->id;
+        ]);
+        $validated['user_id'] = $user->id;
 
-        //$comment = Comment::create($validated);
+        $comment = Comment::create($validated);
 
         return response()->json([
             'message' => 'Jó lesz!4!',
+            'comment' => $comment,
         ], 201);
     }
 }
