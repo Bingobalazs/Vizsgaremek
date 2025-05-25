bcc4a8d4858616ed579a05e7ac01b869b71f4838
KCsaba1 <cskovacs4@gmail.com>
2025-05-25 20:14:48 +0200

    Update CommentController.php

--- backend/app/Http/Controllers/CommentController.php
+++ backend/app/Http/Controllers/CommentController.php
@@ -38,7 +38,7 @@ class CommentController extends Controller
 
     public function addComment(Request $request)
     {
-        /*$validated = $request->validate([
+        $validated = $request->validate([
             'post_id' => 'required|integer',
             'comment' => 'required|string',
             'user_id' => 'required|integer',
@@ -50,8 +50,6 @@ class CommentController extends Controller
         return response()->json([
             'message' => 'Jó lesz!4!',
             'comment' => $comment,
-        ], 201);*/
-
-        return "life could be dream";
+        ], 201);
     }
 }
